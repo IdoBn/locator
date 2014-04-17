079204de38b55bfd3b3f37f7bc0a360f4a49018a
Ido Ben-Natan <ido.bennatan@gmail.com>
2014-04-17 18:22:59 +0300

    fixing reverse geocoded by

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -1,6 +1,11 @@
 class Checkin < ActiveRecord::Base
 	belongs_to :user
 
-	reverse_geocoded_by :latitude, :longitude, address: :location
-	after_validation :reverse_geocode, :if => :address_changed?
+	reverse_geocoded_by :latitude, :longitude do |obj, results|
+		obj.address = results.first.formatted_address
+	end
+	after_validation :reverse_geocode
 end
+
+
+# checkins.create(latitude: 40.6891988, longitude: -74.0445167)
